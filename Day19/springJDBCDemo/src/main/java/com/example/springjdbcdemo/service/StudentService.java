aca719b4c980ad3c1edf68382098d6b1073fa87a
Manoj Verma <123manojverma2005@gmail.com>
2026-09-10 18:09:20 +0530

    Initialize Spring JDBC project with CRUD operations for Student entity

---
+++ Day19/springJDBCDemo/src/main/java/com/example/springjdbcdemo/service/StudentService.java
@@ -0,0 +1,37 @@
+package com.example.springjdbcdemo.service;
+
+import com.example.springjdbcdemo.model.Student;
+import com.example.springjdbcdemo.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public void createStudent(Student student) {
+        studentRepository.createStudent(student);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.getStudent();
+    }
+
+    public Student getStudentById(Long id) {
+        return studentRepository.getStudentById(id);
+    }
+
+    public void updateStudent(Student student) {
+        studentRepository.updateStudent(student, student.getId());
+    }
+
+    public void deleteStudent(Long id) {
+        studentRepository.deleteStudent(id);
+    }
+}
